4dc515baf552229759612d639ca21110b7ac072b
UH5555 <zhuhao162@163.com>
2022-05-06 20:30:09 +0800

    0507\动态内存管理通讯录（1）

---
+++ contact.h
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//声明函数
+//#define MAX 1000
+
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+#define DEFAULT_SZ 3
+
+enum Option
+{
+	EXIT,//0
+	ADD,//1
+	DEL,//2
+	SEARCH,//3
+	MODIFY,//4
+	SHOW,//5
+	SORT//6
+};
+
+struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
+
+//通讯录类型
+struct Contact
+{
+	struct PeoInfo *data;//存放1000个信息
+	int size; //记录当前已有的元素个数
+	int capacity;//当前通讯录的最大容量
+};
+
+//声明函数
+//初始化通讯录
+void InitContact(struct Contact* ps);
+//增加联系人
+void AddContact(struct Contact* ps);
+//查看通讯录
+void ShowContact(const struct Contact* ps);
+//删除一个联系人
+void DelContact(struct Contact* ps);
+//查找联系人
+void SearchContact(const struct Contact* ps);
+//修改联系人
+void ModifyContact(const struct Contact* ps);
+//排序通讯录内容
+void SortContact(const struct Contact* ps);
